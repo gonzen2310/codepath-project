638159079650685611c5526d12e259d9639ecf04
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-05-21 06:20:00 -0400

    Add toolbar and bottom navbar

--- Burn/app/src/main/java/com/beginurrev/burn/HomeActivity.java
+++ Burn/app/src/main/java/com/beginurrev/burn/HomeActivity.java
@@ -2,6 +2,7 @@ package com.beginurrev.burn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Set Toolbar for Home activity
+        Toolbar mtoolbar = findViewById(R.id.homeToolbar);
+        setSupportActionBar(mtoolbar);
+
     }
 }
