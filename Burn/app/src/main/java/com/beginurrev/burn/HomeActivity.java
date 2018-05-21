bbd1f67f6ddadbbc142faa49f245f500a6d8932d
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-05-21 06:40:43 -0400

    Add fragment container

--- Burn/app/src/main/java/com/beginurrev/burn/HomeActivity.java
+++ Burn/app/src/main/java/com/beginurrev/burn/HomeActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +16,12 @@ public class HomeActivity extends AppCompatActivity {
         // Set Toolbar for Home activity
         Toolbar mtoolbar = findViewById(R.id.homeToolbar);
         setSupportActionBar(mtoolbar);
+
+        // Set Bottom Navigation Bar
+        BottomNavigationViewEx mbottomNavigationViewEx = findViewById(R.id.bottomNavigationBar);
+        mbottomNavigationViewEx.enableAnimation(false);
+        mbottomNavigationViewEx.enableShiftingMode(false);
+        mbottomNavigationViewEx.enableItemShiftingMode(false);
 
     }
 }
