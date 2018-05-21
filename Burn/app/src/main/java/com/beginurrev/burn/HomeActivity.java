f12156504b8336771589c55f57fdbeecd66947f0
gonzen2310 <gonzaloreyesllauca@gmail.com>
2018-05-21 19:09:36 -0400

    Center toolbar title

--- Burn/app/src/main/java/com/beginurrev/burn/HomeActivity.java
+++ Burn/app/src/main/java/com/beginurrev/burn/HomeActivity.java
@@ -3,19 +3,28 @@ package com.beginurrev.burn;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "HomeActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: Start Home Activity");
 
         // Set Toolbar for Home activity
         Toolbar mtoolbar = findViewById(R.id.homeToolbar);
-        setSupportActionBar(mtoolbar);
+
+        // Get Title from Toolbar
+        TextView toolbarTitle = findViewById(R.id.toolbarTitle);
+        toolbarTitle.setText(getResources().getText(R.string.home_fragment));
+
 
         // Set Bottom Navigation Bar
         BottomNavigationViewEx mbottomNavigationViewEx = findViewById(R.id.bottomNavigationBar);
